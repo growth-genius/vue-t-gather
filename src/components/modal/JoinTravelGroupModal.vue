fcc39ce89b8deaf7c1f9d867e9a36f3232a87466
cyeji <choyeji1591@gmail.com>
2023-07-06 22:08:56 +0900

    feat : 여행그룹 참가 시 프로필 추가 버튼 활성화

--- src/components/modal/JoinTravelGroupModal.vue
+++ src/components/modal/JoinTravelGroupModal.vue
@@ -25,28 +25,90 @@
                     </div>
                 </template>
                 <template #content>
+                    <section v-if="result.dataURL && result.blobURL" class="section q-mx-lg">
+                        <div class="preview">
+                            <img :src="result.dataURL" alt="" style="min-width: 100%" />
+                        </div>
+                    </section>
+                    <div class="flex justify-content-center text-center mb-3">
+                        <label for="profileImage" class="profile-button font-bold text-900 text-white">
+                            프로필 추가
+                        </label>
+                        <div class="align-items-center">
+                            <input type="file" id="profileImage" style="display: none" @change="selectFile($event)" />
+                        </div>
+                    </div>
+                    <image-pre-view
+                        :pic="pic"
+                        :isShowModal="isShowModal"
+                        @cancel:pic="cancelThumbnail"
+                        @update:pic="updatePic"
+                        class="buttonColorGreen"
+                    />
                     <label for="nickname"> 닉네임 : </label>
                     <input-text id="nickname" v-model="v$.nickname.$model" class="w-7" />
                 </template>
                 <template #footer>
-                    <Button label="참여하기" icon="pi pi-check" />
+                    <Button label="참여하기" icon="pi pi-check" style="background-color: #03d069" />
                 </template>
             </Card>
         </div>
     </Dialog>
 </template>
 <script setup>
-import { defineEmits, ref } from 'vue';
+import { defineEmits, reactive, ref } from 'vue';
 import { helpers, required } from '@vuelidate/validators';
 import { useVuelidate } from '@vuelidate/core';
 import { useModalStore } from '@/store/modal';
+import ImagePreView from '@/components/common/ImagePreView.vue';
 
 const modalStore = useModalStore();
 
 const joinTravelGroup = ref({
     nickname: '',
+    profileImage: '',
 });
 
+const isShowModal = ref(false);
+const uploadInput = ref(null);
+const pic = ref('');
+const imageFile = ref(null);
+
+const result = reactive({
+    dataURL: '',
+    blobURL: '',
+});
+
+const selectFile = event => {
+    const file = event.target.files[0];
+    // Reset last selection and results
+    pic.value = '';
+    result.dataURL = '';
+    result.blobURL = '';
+    const reader = new FileReader();
+    reader.readAsDataURL(file);
+    reader.onload = () => {
+        // Update the picture source of the `img` prop
+        pic.value = String(reader.result);
+        // Show the modal
+        isShowModal.value = true;
+        // Clear selected files of input element
+        if (!uploadInput.value) return;
+        uploadInput.value.value = '';
+    };
+};
+
+const updatePic = (dataUrl, blob) => {
+    result.dataURL = dataUrl;
+    result.blobURL = blob;
+    isShowModal.value = false;
+};
+const cancelThumbnail = () => {
+    result.dataURL = '';
+    result.blobURL = '';
+    imageFile.value = null;
+    isShowModal.value = false;
+};
 const cancelJoinTravelGroup = () => {
     emits('cancel:travelGroup');
 };
@@ -57,3 +119,16 @@ const rules = {
 };
 const v$ = useVuelidate(rules, joinTravelGroup);
 </script>
+<style>
+.profile-button {
+    padding: 6px 25px;
+    background-color: #03d069;
+    border-radius: 4px;
+    color: white;
+    cursor: pointer;
+}
+.profile-button:hover {
+    background-color: #03d069;
+    transition: 0.7s;
+}
+</style>
