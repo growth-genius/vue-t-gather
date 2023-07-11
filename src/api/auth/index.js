66bda908f22d16b41e677fc5c13499516b35f702
cyeji <choyeji1591@gmail.com>
2023-07-11 17:29:38 +0900

    feat : 회원가입 페이지 관련 API 추가
    - 회원가입
    - 닉네임 유효값 인증
    - 이메일 유효값 인증

--- src/api/auth/index.js
+++ src/api/auth/index.js
@@ -5,13 +5,28 @@ function signIn(data) {
     return axiosPost('/account/auth/sign-in', data);
 }
 
+// 회원가입
+function signUp(data) {
+    return axiosPost('/account/auth/sign-up', data);
+}
+
 // 토큰 값 인증
 function checkEmailToken(token, email) {
     return axiosGet(`/account/auth/check-email-token/${token}/${email}`);
+}
+
+// 닉네임 유효 값 인증
+function checkNickname(nickname) {
+    return axiosGet(`/account/auth/check-nickname/${nickname}`);
+}
+
+// 이메일 유효 값 인증
+function checkEmail(email) {
+    return axiosGet(`/account/auth/check-email/${email}`);
 }
 
 function getInit() {
     return axiosGet('/account/login-form');
 }
 
-export { signIn, checkEmailToken, getInit };
+export { signIn, checkEmailToken, getInit, checkNickname, signUp, checkEmail };
